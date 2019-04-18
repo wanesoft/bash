44efb3831099b73ad03a397e37c2af5cc9c80adc
wanesoft <wanesoft@mail.ru>
2019-04-18 11:26:36 +0300

    autocompl_v_9

--- sources/ft_new_while.c
+++ sources/ft_new_while.c
@@ -14,32 +14,51 @@
 
 char				g_str[B_SIZE];
 int					g_j = 0;
+int					g_pos = 1;
 
-//static void			ft_hello_mess(char *str, unsigned long i)
-//{
-//	if (i != K_LEFT)
-//	{
-//		ft_putstr_fd("\033[36mWelcome>\033[0m", TESTTT);
-//		ft_putstr_fd(str, TESTTT);
-//	}
-//}
+static void			ft_hello_mess(char *str)
+{
+	int				tmp;
+	
+	tmp = g_pos;
+	ft_putstr_fd(tgetstr("dl", NULL), TESTTT);
+	ft_putstr_fd(tgetstr("cr", NULL), TESTTT);
+	ft_putstr_fd("\033[36mWelcome>\033[0m", TESTTT);
+	ft_putstr_fd(str, TESTTT);
+	while (g_j > tmp)
+	{
+		ft_putstr_fd(tgetstr("le", NULL), TESTTT);
+		++tmp;
+	}
+}
+
+static void			ft_i_anoth()
+{
+	int i = 0;
+	++i;
+}
 
 static void			ft_put_let(char *str, int *g_j, unsigned long i)
 {
-	if (!i)
+	char			tmp;
+	char			tmp2;
+	int				itmp;
+	
+	itmp = g_pos;
+	tmp = str[g_pos];
+	str[g_pos] = i;
+	while (itmp < *g_j)
 	{
-		//ft_putstr_fd(tgoto(tgetstr("cm", NULL), ((*g_j) - g_jj), 50), TESTTT);
-//		ft_putstr_fd(tgetstr("dl", NULL), TESTTT);
-		ft_putstr_fd(tgetstr("le", NULL), TESTTT);
-		return ;
+		tmp2 = str[itmp + 1];
+		str[itmp] = tmp;
+		tmp = tmp2;
+		++itmp;
 	}
-	str[*g_j] = i;
-	str[*g_j + 1] = '\0';
 	*g_j += 1;
-	//ft_putstr_fd(str, TESTTT);
-	ft_putstr_fd(tgetstr("dl", NULL), TESTTT);
-	ft_putstr_fd(str, TESTTT);
-	//ft_putstr_fd(tgetstr("cr", NULL), TESTTT);
+	++g_pos;
+	str[*g_j] = '\0';
+	ft_hello_mess(str);
+	
 }
 
 static void			ft_i_enter(char *str)
@@ -55,6 +74,14 @@ static void			ft_i_enter(char *str)
 }//	str[0] = '\0';
 //	*g_j = 0;
 
+static void			ft_i_arrows(unsigned long i)
+{
+	if (i == K_LEFT && g_pos > 0)
+		--g_pos;
+	else if (i == K_RIGHT && g_pos <= g_j)
+		++g_pos;
+	ft_hello_mess(g_str);
+}
 
 static void			ft_i_bksp(char *str, int *g_j)
 {
@@ -65,22 +92,25 @@ static void			ft_i_bksp(char *str, int *g_j)
 	}
 	ft_putstr_fd(tgetstr("dl", NULL), TESTTT);
 	ft_putstr_fd(tgetstr("cr", NULL), TESTTT);
+	ft_hello_mess(str);
 }
 
-char				*ft_new_while()
+char				*ft_new_while(void)
 {
 	unsigned long	i;
 
 	i = 0;
-	g_str[0] = '\0';
+	ft_bzero(g_str, B_SIZE);
+	//g_str[0] = '\0';
 	g_j = 0;
-	//ft_hello_mess(g_str, i);
+	g_pos = 0;
+	ft_hello_mess(g_str);
 	while (1)
 	{
 		signal(SIGINT, ft_main_sig);
 		//ft_hello_mess(g_str, i);
 		read(STDIN_FILENO, &i, 8);
-		
+		//printf("%lu\n", i);
 		if (i == '\n')
 		{
 			ft_i_enter(g_str);
@@ -92,14 +122,13 @@ char				*ft_new_while()
 			ft_autocompl(g_str, &g_j);
 		else if (i == K_BACKSP)
 			ft_i_bksp(g_str, &g_j);
-		else if (i == K_LEFT)
-			ft_put_let(g_str, &g_j, i);
+		else if (i == K_LEFT || i == K_RIGHT)
+			ft_i_arrows(i);
 		else if (ft_isprint((int)i))
 			ft_put_let(g_str, &g_j, i);
 		else
-			ft_put_let(g_str, &g_j, 0);
+			ft_i_anoth();
 		i = 0;
-		
 	}
 	return (0);
 }
