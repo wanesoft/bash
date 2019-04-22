892cc887f346565ed0bd749efb8ec716435e64f7
Giant Gwin goodbrother <ggwin-go@at-r8.21-school.ru>
2019-04-22 15:36:13 +0300

    testing dup2 and pipe (do not work from 21sh yet)

--- sources/pipes_or_redirections/dup2.c
+++ sources/pipes_or_redirections/dup2.c
@@ -6,30 +6,86 @@
 /*   By: ggwin-go <ggwin-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/17 22:36:40 by ggwin-go          #+#    #+#             */
-/*   Updated: 2019/04/18 16:53:15 by ggwin-go         ###   ########.fr       */
+/*   Updated: 2019/04/22 00:25:25 by ggwin-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "21sh.h"
 
-int     handle_redirections(char *filename, char *path, char **av, char **env)
+static	int		write_to_file(int fd, char *path, char **av, char **env)
 {
-    int fd;
-    int ret;
+	(void)av;
+	(void)env;
+	ft_putendl("Write to file:");
+	write(fd, path, ft_strlen(path));
+	return (0);
+}
 
-    fd = open(filename, O_CREAT | O_APPEND | O_WRONLY);
-    if (fd < 0)
-    {
-        perror("open");
-        exit(1);
-    }
-    ret = dup2(fd, 1);
-    if (ret < 0)
-    {
-        perror("dup2");
-        exit(1);
-    }
-    call_exec(path, av, env);
-    close(fd);
-    return (1);
+static	int		read_from_file(int fd, char *path, char **av, char **env)
+{
+	ssize_t		ret;
+	char		*s;
+	char		buf[BUF_SIZE + 1];
+
+	(void)av;
+	(void)env;
+	(void)path;
+	ft_putendl("Read from file:");
+	s = ft_strnew(0);
+	while ((ret = read(fd, buf, BUF_SIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		s = ft_strjoin_free(s, buf, 1);
+	}
+	ft_putendl(s);
+	return (0);
+}
+
+int				handle_redirections(char *filename, char *direct,
+									char *path, char **av, char **env)
+{
+	int fd;
+	// int ret;
+	++direct;
+	if (*direct == '>')
+	{
+		if (*(direct + 1) == '>')
+			fd = open(filename, O_CREAT | O_APPEND | O_WRONLY, 644);
+		else
+			fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 644);
+	}
+	else
+	{
+		fd = open(filename, O_CREAT | O_APPEND | O_RDONLY);
+	}	
+	if (fd < 0)
+	{
+		perror("open");
+		exit(1);
+	}
+	// ret = dup2(fd, 1);
+	if (*direct == '>')
+		write_to_file(fd, path, av, env);
+	else
+		read_from_file(fd, path, av, env);
+	// if (ret < 0)
+	// {
+	// 	perror("dup2");
+	// 	exit(1);
+	// }
+	// call_exec(path, av, env);
+	close(fd);
+	return (1);
+}
+
+int				main(int ac, char **av, char **env)
+{
+	if (ac > 3)
+	{
+		(void)av;
+		(void)env;
+		++av;
+		handle_redirections(*av, *(av + 1), *(av + 2), av + 2, env);
+	}
+	return (0);
 }
--- sources/pipes_or_redirections/handle_pipe.c
+++ sources/pipes_or_redirections/handle_pipe.c
@@ -6,13 +6,33 @@
 /*   By: ggwin-go <ggwin-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/13 13:42:48 by ggwin-go          #+#    #+#             */
-/*   Updated: 2019/04/17 22:40:28 by ggwin-go         ###   ########.fr       */
+/*   Updated: 2019/04/19 23:46:01 by ggwin-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "21sh.h"
 
-int		handle_pape(char **s1, char **s2, char **res, char **env)
+int	call_exec(char *path, char **split, char **env)
+{
+	pid_t	my_pid;
+
+	if (!access(path, X_OK))
+	{
+		my_pid = fork();
+		if (my_pid > 0)
+			wait(NULL);
+		if (my_pid == 0)
+			execve(path, split, env);
+	}
+	else
+	{
+		ft_putstr("21sh: permission denied: ");
+		ft_putendl(path);
+	}
+	return (1);
+}
+
+int		handle_pipe(char **s1, char **s2, char **res, char **env)
 {
 	pid_t	childe_pid;
 	ssize_t	rd;
@@ -41,8 +61,8 @@ int		handle_pape(char **s1, char **s2, char **res, char **env)
 		wait(NULL);
 		printf("\nChilde:\n\n");
 		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
 		call_exec(*s2, s2 + 1, env);
+		close(fd[0]);
 		// write(fd[1], *res, ft_strlen(*s1));
 	}
 	else
@@ -72,13 +92,14 @@ int		handle_pape(char **s1, char **s2, char **res, char **env)
 
 int     main(int ac, char **av, char **env)
 {
+	// char	*s1[4] = {"/bin/cat", "cat", "author", NULL};
 	char	*s1[4] = {"/bin/ls", "ls", ".", NULL};
-	char	*s2[4] = {"/bin/cat", "cat", NULL, NULL};
+	char	*s2[4] = {"/bin/ls", "ls", ".", NULL};
 	char	*res;
 
 	res = NULL;
 	printf("%s\n", res);
-    handle_pape(s1, s2, &res, env);
+    handle_pipe(s1, s2, &res, env);
 	printf("\n%s\n", res);
     return (0);
 }
